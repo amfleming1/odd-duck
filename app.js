6ed353d68a94983c8efc887a88673c99dd2949d5
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-06-03 21:17:24 +0000

    added javascript

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let votingArea = document.getElementById("voting-area");
+let resultsArea = doucment.getElementbyId("results-area");
+
+let dogImg = document.getElementbyId("Product1");
+let bootsImg = document.getElelmentbyId("Product2");
+let bananaImg = document.getElementbyId("Product3");
+
+//products constructor
+function Products(name, scr){
+    this.name =name;
+    this.src= src;
+    this.clicks = 0;
+    this.views = 0;
+}
+
+let dog = new Products("dog", )
+
+
